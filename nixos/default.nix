274cb7241ff35b8b3cb4bdc69cc86168ec164e1a
Chrisser1 <97701175+Chrisser1@users.noreply.github.com>
2025-08-07 19:13:24 +0200

    Update default.nix
    Added vm

--- nixos/default.nix
+++ nixos/default.nix
@@ -5,8 +5,12 @@
   ...
 }: {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+
+  # Bootloader for VM
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
 
   # Nix flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
