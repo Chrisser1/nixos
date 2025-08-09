59569a1353a5d4b4e5964b21396c43c22047f063
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-08-09 22:24:52 +0200

    Added extensions to firefox

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,15 +1,20 @@
-{ 
-    pkgs, 
-    ... 
-}:
+{ pkgs, inputs, ... }:
+
+let
+  # This makes sure we are using the 'nixpkgs' flake input you defined
+  # and not some other version of pkgs that might be getting passed accidentally.
+  firefox-addons = inputs.nixpkgs.legacyPackages.${pkgs.system}.firefox-addons;
+in
 {
   programs.firefox = {
     enable = true;
-    # Let Nix manage the extensions.
-    extensions = with pkgs.firefox-addons; [
-      ublock-origin
-      bitwarden
-      grammarly
-    ];
+    profiles.chris = {
+      isDefault = true;
+      extensions.packages = [
+        firefox-addons.ublock-origin
+        firefox-addons.bitwarden
+        firefox-addons.grammarly
+      ];
+    };
   };
 }
