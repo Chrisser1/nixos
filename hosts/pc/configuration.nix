8522cc1e891fccc503063607ad9fb8b1bb9a85f7
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-09-01 19:37:04 +0200

    Added grub as boot

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -49,6 +49,10 @@
   };
 
   # Bootloader (add what you actually use on the PC)
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 }
