3bd12fa90f7c3a680bc1db14ea5abf9a2ca23191
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-09-24 11:23:45 +0200

    Added some more nvim

--- modules/nixos/dev.nix
+++ modules/nixos/dev.nix
@@ -14,8 +14,15 @@
   ##### NVF (Neovim config)
   programs.nvf = {
     enable = true;
+    defaultEditor = true;
+
     settings = {
       vim = {
+        startPlugins = with pkgs.vimPlugins; [
+          nvim-comment
+          multicursor-nvim
+        ];
+
         visuals.nvim-cursorline = {
           enable = true;
           setupOpts.cursorline = {
@@ -54,8 +61,112 @@
         };
 
         keymaps = [
+          # --- Multi-Cursor (doesn't currently work needs to be fixed) ---
           {
-            mode = "n"; # Normal mode
+            mode = [ "n" "v" ];
+            key = "<C-A-Down>";
+            action = ":lua require('multicursor-nvim').lineAddCursor(1)<CR>";
+            desc = "Multi-Cursor: Add cursor below";
+          }
+          {
+            mode = [ "n" "v" ];
+            key = "<C-A-Up>";
+            action = ":lua require('multicursor-nvim').lineAddCursor(-1)<CR>";
+            desc = "Multi-Cursor: Add cursor above";
+          }
+          {
+            mode = [ "n" "v" ];
+            key = "<C-d>";
+            action = ":lua require('multicursor-nvim').matchAddCursor(1)<CR>";
+            desc = "Multi-Cursor: Select next match";
+          }
+
+          # --- Select with Shift + Arrow ---
+          # Normal Mode
+          { mode = "n"; key = "<S-Up>";    action = "v<Up>";    desc = "Select line up"; }
+          { mode = "n"; key = "<S-Down>";  action = "v<Down>";  desc = "Select line down"; }
+          { mode = "n"; key = "<S-Left>";  action = "v<Left>";  desc = "Select left"; }
+          { mode = "n"; key = "<S-Right>"; action = "v<Right>"; desc = "Select right"; }
+
+          # Insert Mode
+          { mode = "i"; key = "<S-Up>";    action = "<Esc>v<Up>";    desc = "Select line up"; }
+          { mode = "i"; key = "<S-Down>";  action = "<Esc>v<Down>";  desc = "Select line down"; }
+          { mode = "i"; key = "<S-Left>";  action = "<Esc>v<Left>";  desc = "Select left"; }
+          { mode = "i"; key = "<S-Right>"; action = "<Esc>v<Right>"; desc = "Select right"; }
+
+          # Visual Mode
+          { mode = "v"; key = "<S-Up>";    action = "<Up>";    desc = "Select line up"; }
+          { mode = "v"; key = "<S-Down>";  action = "<Down>";  desc = "Select line down"; }
+          { mode = "v"; key = "<S-Left>";  action = "<Left>";  desc = "Select left"; }
+          { mode = "v"; key = "<S-Right>"; action = "<Right>"; desc = "Select right"; }
+
+          # --- Copy Line Up/Down ---
+          {
+            mode = "n";
+            key = "<A-S-Down>";
+            action = "yyp"; # Yank line and paste below
+            desc = "Copy line down";
+          }
+          {
+            mode = "v";
+            key = "<A-S-Down>";
+            action = "y'>p"; # Yank selection and paste below
+            desc = "Copy selection down";
+          }
+          {
+            mode = "n";
+            key = "<A-S-Up>";
+            action = "Yp"; # Yank line and paste above
+            desc = "Copy line up";
+          }
+          {
+            mode = "v";
+            key = "<A-S-Up>";
+            action = "y'<P"; # Yank selection and paste above
+            desc = "Copy selection up";
+          }
+
+          # --- Move Line Up/Down ---
+          {
+            mode = "n";
+            key = "<A-Down>";
+            action = "<cmd>m .+1<cr>==";
+            desc = "Move line down";
+          }
+          {
+            mode = "v";
+            key = "<A-Down>";
+            action = ":m '>+1<cr>gv=gv";
+            desc = "Move selection down";
+          }
+          {
+            mode = "n";
+            key = "<A-Up>";
+            action = "<cmd>m .-2<cr>==";
+            desc = "Move line up";
+          }
+          {
+            mode = "v";
+            key = "<A-Up>";
+            action = ":m '<-2<cr>gv=gv";
+            desc = "Move selection up";
+          }
+          
+          # --- Other Mappings ---
+          {
+            mode = "n";
+            key = "<C-/>";
+            action = "gcc";
+            desc = "Comment: Toggle line";
+          }
+          {
+            mode = "v";
+            key = "<C-/>";
+            action = "gc";
+            desc = "Comment: Toggle selection";
+          }
+          {
+            mode = "n";
             key = "<leader>ff";
             action = "<cmd>Telescope find_files<cr>";
             desc = "Telescope: Find Files";
