d82410e2cf0689b6e8e6af33d6eda3e0ee769594
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-09-25 09:33:49 +0200

    Trying to fix vs code

--- modules/home/dev/vscode.nix
+++ modules/home/dev/vscode.nix
@@ -1,48 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
     mutableExtensionsDir = true;
     
-    profiles.default = {
-      enableUpdateCheck = false;
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        mkhl.direnv
-      ];
-      userSettings = {
-        "files.autoSave" = "afterDelay";
-        "files.autoSaveDelay" = 1000;
-        "disable-hardware-acceleration" = true;
-      };
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      mkhl.direnv
+    ];
+  };
 
-      keybindings = [
-        { "key" = "ctrl+'"; "command" = "editor.action.accessibleViewAcceptInlineCompletion"; "when" = "accessibleViewIsShown && accessibleViewCurrentProviderId == 'inlineCompletions'"; }
-        { "key" = "ctrl+shift+7"; "command" = "-editor.action.accessibleViewAcceptInlineCompletion"; "when" = "accessibleViewIsShown && accessibleViewCurrentProviderId == 'inlineCompletions'"; }
-        { "key" = "ctrl+'"; "command" = "github.copilot.acceptCursorPanelSolution"; "when" = "github.copilot.panelVisible && activeWebviewPanelId == 'GitHub Copilot Suggestions'"; }
-        { "key" = "ctrl+shift+7"; "command" = "-github.copilot.acceptCursorPanelSolution"; "when" = "github.copilot.panelVisible && activeWebviewPanelId == 'GitHub Copilot Suggestions'"; }
-        { "key" = "ctrl+'"; "command" = "notebook.commentSelectedCells"; "when" = "notebookEditable && notebookEditorFocused && !inputFocus"; }
-        { "key" = "ctrl+shift+7"; "command" = "-notebook.commentSelectedCells"; "when" = "notebookEditable && notebookEditorFocused && !inputFocus"; }
-        { "key" = "ctrl+'"; "command" = "editor.action.commentLine"; "when" = "editorTextFocus && !editorReadonly"; }
-        { "key" = "ctrl+shift+7"; "command" = "-editor.action.commentLine"; "when" = "editorTextFocus && !editorReadonly"; }
-        { "key" = "ctrl+'"; "command" = "terminalSuggestToggleExplainMode"; "when" = "terminalFocus && terminalHasBeenCreated && terminalIsOpen && terminalSuggestWidgetVisible || terminalFocus && terminalIsOpen && terminalProcessSupported && terminalSuggestWidgetVisible"; }
-        { "key" = "ctrl+shift+7"; "command" = "-terminalSuggestToggleExplainMode"; "when" = "terminalFocus && terminalHasBeenCreated && terminalIsOpen && terminalSuggestWidgetVisible || terminalFocus && terminalIsOpen && terminalProcessSupported && terminalSuggestWidgetVisible"; }
-        { "key" = "ctrl+'"; "command" = "toggleExplainMode"; "when" = "suggestWidgetVisible"; }
-        { "key" = "ctrl+shift+7"; "command" = "-toggleExplainMode"; "when" = "suggestWidgetVisible"; }
-        { "key" = "ctrl+'"; "command" = "workbench.action.chat.attachContext"; "when" = "inChatInput && chatLocation == 'panel'"; }
-        { "key" = "ctrl+shift+7"; "command" = "-workbench.action.chat.attachContext"; "when" = "inChatInput && chatLocation == 'panel'"; }
-        { "key" = "ctrl+'"; "command" = "workbench.action.terminal.sendSequence"; "when" = "terminalFocus"; }
-        { "key" = "ctrl+shift+7"; "command" = "-workbench.action.terminal.sendSequence"; "when" = "terminalFocus"; }
-        { "key" = "ctrl+alt+up"; "command" = "editor.action.insertCursorAbove"; "when" = "editorTextFocus"; }
-        { "key" = "ctrl+shift+up"; "command" = "-editor.action.insertCursorAbove"; "when" = "editorTextFocus"; }
-        { "key" = "ctrl+alt+down"; "command" = "editor.action.insertCursorBelow"; "when" = "editorTextFocus"; }
-        { "key" = "ctrl+shift+down"; "command" = "-editor.action.insertCursorBelow"; "when" = "editorTextFocus"; }
-        { "key" = "shift+alt+down"; "command" = "editor.action.copyLinesDownAction"; "when" = "editorTextFocus && !editorReadonly"; }
-        { "key" = "ctrl+shift+alt+down"; "command" = "-editor.action.copyLinesDownAction"; "when" = "editorTextFocus && !editorReadonly"; }
-        { "key" = "shift+alt+up"; "command" = "editor.action.copyLinesUpAction"; "when" = "editorTextFocus && !editorReadonly"; }
-        { "key" = "ctrl+shift+alt+up"; "command" = "-editor.action.copyLinesUpAction"; "when" = "editorTextFocus && !editorReadonly"; }
-      ];
-    };
+  home.file.".config/Code/User/settings.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/modules/home/dev/vscode-settings.json";
+  };
+
+  # This does the same for your keybindings.
+  home.file.".config/Code/User/keybindings.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/modules/home/dev/vscode-keybindings.json";
   };
 }
