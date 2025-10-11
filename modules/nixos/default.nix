0263751c78069144d1e7763f911ba803760e25c3
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-10-11 20:26:21 +0200

    adding custom sddm

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./base.nix
     ./desktop.nix
+    ./sddm.nix
   ];
 }
--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -12,12 +12,6 @@
     xwayland.enable = true;
   };
 
-  services.displayManager = {
-    sddm.enable = true;
-    sddm.wayland.enable = true;
-    defaultSession = "hyprland";
-  };
-
   # Portals for Wayland apps (Hyprland + GTK)
   xdg.portal = {
     enable = true;
