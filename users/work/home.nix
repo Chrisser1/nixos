ca9b781b982b6b50397c5f80c6b0c1b717f429ec
Chrisser1 <chrisgthomsen0310@gmail.com>
2025-11-17 15:58:31 +0100

    Add user directories and systemd services for mounting Wavepiston folders

--- users/work/home.nix
+++ users/work/home.nix
@@ -20,5 +20,46 @@
     dbeaver-bin
 
     rclone
+    fuse3
   ];
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    # We can just list our mount points here
+    extraConfig = {
+      XDG_SHAREPOINT_TEAM_DIR = "$HOME/WavepistonTeamFolder";
+      XDG_SHAREPOINT_DATA_DIR = "$HOME/WavepistonDataWarehouse";
+    };
+  };
+
+  systemd.user.services = {
+    WavepistonTeamFolder-mount = {
+      Unit = {
+        Description = "Mount Wavepiston Team Folder";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.rclone}/bin/rclone mount WavepistonTeamFolder: %h/WavepistonTeamFolder --vfs-cache-mode writes";
+        ExecStop = "${pkgs.fuse3}/bin/fusermount3 -u %h/WavepistonTeamFolder";
+        Restart = "on-failure";
+      };
+    };
+
+    WavepistonDataWarehouse-mount = {
+      Unit = {
+        Description = "Mount Wavepiston DataWarehouse";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.rclone}/bin/rclone mount WavepistonDataWarehouse: %h/WavepistonDataWarehouse --vfs-cache-mode writes";
+        ExecStop = "${pkgs.fuse3}/bin/fusermount3 -u %h/WavepistonDataWarehouse";
+        Restart = "on-failure";
+      };
+    };
+  };
 }
