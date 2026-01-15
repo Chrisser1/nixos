ebee33c5b02ea7d977a5fabd662cd589247bce62
Chrisser1 <chrisgthomsen0310@gmail.com>
2026-01-15 08:58:51 +0100

    Java works now

--- modules/home/dev/java.nix
+++ modules/home/dev/java.nix
@@ -12,5 +12,17 @@
 
   home.sessionVariables = {
     JAVA_HOME = "${pkgs.jdk25}/lib/openjdk";
+
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.xorg.libXxf86vm
+      pkgs.xorg.libXtst
+      pkgs.libglvnd
+      pkgs.gtk3             
+      pkgs.glib
+      pkgs.cairo
+      pkgs.pango
+      pkgs.atk
+      pkgs.gdk-pixbuf
+    ];
   };
 }
