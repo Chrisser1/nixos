f8f2a5f283b653ba4237b758193d70ac0fa6d111
Chrisser1 <chrisgthomsen0310@gmail.com>
2026-01-25 20:06:04 +0100

    small change to git

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,9 +1,15 @@
 {
 	programs.git = {
 		enable = true;
-		settings.user = {
-			name = "Chrisser1";
-			email = "chrisgthomsen0310@gmail.com";
+		settings = {
+			user = {
+				name = "Chrisser1";
+				email = "chrisgthomsen0310@gmail.com";
+			};
+
+			url."git@github.com:" = {
+				insteadOf = "https://github.com/";
+			};
 		};
 	};
 }
