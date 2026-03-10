ec9c5a14fb4117b8f63e352a2f1ff4bb3d15c752
Chrisser1 <chrisgthomsen0310@gmail.com>
2026-03-10 08:31:05 +0100

    Cleaned up laptop and removed swap as i have auto swap with nixos

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -24,10 +24,6 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/6c6a51b5-57d4-4828-8c5f-233fb97b67d8"; }
-    ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
