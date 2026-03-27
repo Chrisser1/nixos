5b27baccc384dd4b0f2f8feb1a6ed987cc524212
Chrisser1 <chrisgthomsen0310@gmail.com>
2026-03-27 20:31:28 +0100

    wd

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,35 +1,23 @@
-{ self, inputs, ... }: {
+ { self, inputs, ... }: {
+
   perSystem = { pkgs, lib, ... }: {
+
     packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+
       inherit pkgs;
 
-      binName = "noctalia"; 
+      settings = lib.recursiveUpdate (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings {
 
-      settings = let
-        screenShotSrc = pkgs.fetchFromGitHub {
-          owner = "Chrisser1";
-          repo = "noctalia-screen-shot-and-record";
-          rev = "master"; 
-          hash = "sha256-62x7NfyL1zDFQrh5dZuTAM+zH1I5lRLvctAJZbWhyL8=";
-        };
+        # Dynamically copying assets to the Nix store
 
-        screenShotPlugin = pkgs.runCommand "format-plugin-dir" {} ''
-          mkdir -p $out/screen-shot-and-record
-          cp -r ${screenShotSrc}/* $out/screen-shot-and-record/
-        '';
+        general.avatarImage = "${../../assets/backgrounds/berserk/Knight.png}";
 
-        baseConfig = builtins.fromJSON (builtins.readFile ./noctalia.json);
-      in 
-        lib.recursiveUpdate baseConfig.settings {
-          general.avatarImage = "${../../assets/backgrounds/berserk/Knight.png}";
-          wallpaper.directory = "${../../assets/backgrounds}";
+        wallpaper.directory = "${../../assets/backgrounds}";
 
-          plugins.screen-shot-and-record = {
-            enabled = true;
-            sourceUrl = "local";
-            path = "${screenShotPlugin}/screen-shot-and-record";
-          };
-        };
+      };
+
     };
+
   };
-}
+
+} 
