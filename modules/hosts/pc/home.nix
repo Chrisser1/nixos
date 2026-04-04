a0819553e61d66f26992d0d42d8311135f2e6e17
Chrisser1 <chrisgthomsen0310@gmail.com>
2026-04-04 10:11:52 +0200

    Updated monitors

--- modules/hosts/pc/home.nix
+++ modules/hosts/pc/home.nix
@@ -5,13 +5,13 @@
     wayland.windowManager.hyprland.settings = {
       monitor = [
         # Middle monitor (ASUS)
-        "DP-4,highrr,0x0,1"
+        "DP-1,highrr,0x0,1"
         
         # Left monitor (HP)
-        "HDMI-A-1,highrr,auto-left,1"
+        "HDMI-A-2,highrr,auto-left,1"
         
         # Right monitor (HP)
-        "HDMI-A-2,highrr,auto-right,1" 
+        "HDMI-A-1,highrr,auto-right,1" 
       ];
     };
   };
